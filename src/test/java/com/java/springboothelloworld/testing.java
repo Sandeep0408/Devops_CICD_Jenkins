03e2ae9e47efe91e405f662d50d8dcb6b68fd1a7
Sandeep kumar <sandeepkumar@git.com>
2021-11-29 14:42:24 +0530

    Adding test file

---
+++ src/test/java/com/java/springboothelloworld/testing.java
@@ -0,0 +1,13 @@
+class Calculator {
+    
+    public static int add(int x, int y){
+      return x + y;
+    }
+
+}
+
+@Test
+public void testAdd() throws Exception {
+  int result = Calculator.add(10, 20);
+  assertEquals(30, result);
+}
